a722e6bef3d4fab79872210a3b8c80a59ef34578
Anastasiia Sorina <anastasiiasorina@gmail.com>
2024-02-22 09:57:51 +0000

    add redme

---
+++ src/components/home-page/р.js
@@ -0,0 +1,18 @@
+// Завдання 4
+// Створити функцію calculator яка буде працювати як зазначено нижче:
+
+var calc = calculator();
+
+calc.number("4"); // 4
+
+calc.plus(); // +
+
+calc.number("7"); // 7
+
+calc.number("3"); // 3
+
+calc.minus(); // -
+
+calc.number("2"); // 2
+
+calc.eq(); // 75
